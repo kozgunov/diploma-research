0b8aed860fc7018602fbe0df80b1220dcd6258be
kozgunov <78970376+kozgunov@users.noreply.github.com>
2024-09-27 02:28:56 +0300

    Add files via upload

---
+++ local training.py
@@ -0,0 +1,13 @@
+async def local_training(self, epochs=1):
+    self.initialize_model_state()
+    self.model.train()
+    for epoch in range(epochs):
+        for inputs, labels in self.data_loader():
+            self.optimizer.zero_grad()
+            outputs = self.model(inputs)
+            loss = self.loss_fn(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+    delta_w = self.get_model_updates()
+    encrypted_updates = self.encrypt_updates(delta_w)
+    return encrypted_updates
