7e91a3b07ed4791798bce20366a70be75a124361
kozgunov <78970376+kozgunov@users.noreply.github.com>
2024-10-02 00:54:16 +0300

    Create blockchain_structure.py

---
+++ version_2/blockchain_structure.py
@@ -0,0 +1,12 @@
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+
+    def add_block(self, block):
+        self.chain.append(block)
+
+    def validate_chain(self):
+        pass
+
+
+# in process
